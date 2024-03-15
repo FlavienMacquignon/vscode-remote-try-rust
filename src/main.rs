2753dffd81e924f047b38d09e05b66d058d8ed2f
FlavienMacquignon <70152975+FlavienMacquignon@users.noreply.github.com>
2024-03-15 22:10:49 +0000

    WIP RUST-001

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,23 @@
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
  *-------------------------------------------------------------------------------------------------------------*/
 
-fn main() {
-    let name = "VS Code Remote - Containers";
-    println!("Hello, {}!", name);
-}
+ use std::io;
+ use rand::thread_rng;
+
+
+ fn main() {    
+      println!("Guess the number!");
+ 
+     let secret_number = rand::thread_rng().gen_range(1..=100);
+ 
+     println!("The secret number is: {secret_number}");
+ 
+     println!("Please input your guess.");
+ 
+     let mut guess = String::new();
+ 
+     io::stdin()
+         .read_line(&mut guess)
+         .expect("Failed to read line");
+   println!("You guessed: {guess}");
+ }
